66e195b62e060d99fc6cc3610554d38118c58a92
rahulspoudel <13036329+rahulspoudel@users.noreply.github.com>
2024-08-28 09:19:55 +0545

    update-optimize: putting both element selectors in one place and both to onload

--- scripts/content.js
+++ scripts/content.js
@@ -1,17 +1,9 @@
-const block_section = document.querySelectorAll(
-  "#contents > ytd-rich-section-renderer",
-);
-
-if (block_section.length > 0) {
-  block_section.forEach((element) => (element.style.display = "none"));
-}
-
 window.onload = function () {
-  const in_focused_video_url_shorts = document.querySelectorAll(
-    "#contents > ytd-reel-shelf-renderer",
+  const shorts_sections = document.querySelectorAll(
+    "#contents > ytd-rich-section-renderer, #contents > ytd-reel-shelf-renderer",
   );
 
-  if (in_focused_video_url_shorts.length > 0) {
-    in_focused_video_url_shorts.forEach((element) => element.remove());
+  if (shorts_sections.length > 0) {
+    shorts_sections.forEach((element) => (element.style.display = "none"));
   }
 };
