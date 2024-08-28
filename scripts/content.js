ed0d55f9b6532f83a79a4989001c40366025c40e
rahulspoudel <13036329+rahulspoudel@users.noreply.github.com>
2024-08-28 09:16:08 +0545

    feature-update: added blocking shorts for in video view, more targeted element for both

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,17 @@
-const block_section = document.querySelectorAll("#dismissible #contents");
+const block_section = document.querySelectorAll(
+  "#contents > ytd-rich-section-renderer",
+);
 
 if (block_section.length > 0) {
   block_section.forEach((element) => (element.style.display = "none"));
 }
+
+window.onload = function () {
+  const in_focused_video_url_shorts = document.querySelectorAll(
+    "#contents > ytd-reel-shelf-renderer",
+  );
+
+  if (in_focused_video_url_shorts.length > 0) {
+    in_focused_video_url_shorts.forEach((element) => element.remove());
+  }
+};
